ada150e54b0a5b7e3691f3a4b865d30b63170f71
TinhNguyenTB <tinh271103@gmail.com>
2025-03-05 21:32:31 +0700

    menu items

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,6 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-    title: "School Management Dashboard",
-    description: "Next.js School Management System",
-};
+import Menu from "@/components/Menu";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function DashboardLayout({
     children,
@@ -14,9 +8,20 @@ export default function DashboardLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div>
-            Dashboard
-            {children}
+        <div className="h-screen flex">
+            {/* left */}
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+                <Link href={"/"} className="flex items-center justify-center lg:justify-start gap-2">
+                    <Image src={"/logo.png"} alt="logo" width={32} height={32} />
+                    <span className="hidden lg:block">School</span>
+                </Link>
+                <Menu />
+            </div>
+
+            {/* right */}
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">
+                right
+            </div>
         </div>
     );
 }
