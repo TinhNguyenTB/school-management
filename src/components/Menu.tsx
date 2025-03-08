aa7dabacf08e2f8dca10686a2a8e9a7b7581cab5
TinhNguyenTB <tinh271103@gmail.com>
2025-03-08 13:10:32 +0700

    Schedule Calendar Design

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,5 @@
 import { menuItems } from "@/constants/menuItems"
+import { role } from "@/lib/data"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -11,16 +12,22 @@ const Menu = () => {
             <span className="hidden lg:block text-gray-400 font-light my-4">
               {i.title}
             </span>
-            {i.items.map(item => (
-              <Link key={item.icon} href={item.label}
-                className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
-              >
-                <Image src={item.icon} alt={item.label} width={20} height={20} />
-                <span className="hidden lg:block">
-                  {item.label}
-                </span>
-              </Link>
-            ))}
+            {i.items.map(item => {
+              if (item.visible.includes(role)) {
+                return (
+                  (
+                    <Link key={item.icon} href={item.label}
+                      className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-mySkyLight"
+                    >
+                      <Image src={item.icon} alt={item.label} width={20} height={20} />
+                      <span className="hidden lg:block">
+                        {item.label}
+                      </span>
+                    </Link>
+                  )
+                )
+              }
+            })}
           </div>
         )
       })}
