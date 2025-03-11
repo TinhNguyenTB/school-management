49c181d33d349c089d4f57625027449227afb885
TinhNguyenTB <tinh271103@gmail.com>
2025-03-11 21:33:42 +0700

    Form Modal Design

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from "next/image";
+import { useState } from "react";
 
 type FormModalProps = {
     table: "teacher"
@@ -23,11 +24,39 @@ type FormModalProps = {
 const FormModal = ({ table, type, data, id }: FormModalProps) => {
     const size = type === 'create' ? 'w-8 h-8' : 'w-7 h-7';
     const bgColor = type === 'create' ? 'bg-myYellow' : type === 'update' ? 'bg-mySky' : 'bg-myPurple';
+    const [open, setOpen] = useState(false);
+
+    const Form = () => {
+        return type === 'delete' && id ? (
+            <form className="p-4 flex flex-col gap-4">
+                <span className="text-center font-medium">Are you sure you want to delete this {table}?</span>
+                <button className="bg-red-700 text-white px-4 rounded-md border-none w-max self-center">Delete</button>
+            </form>
+        )
+            :
+            "create or update form"
+    }
+
     return (
         <>
-            <button className={`${size} flex items-center justify-center rounded-full ${bgColor}`}>
+            <button
+                onClick={() => setOpen(true)}
+                className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+            >
                 <Image src={`/${type}.png`} alt="" width={16} height={16} />
             </button>
+            {open &&
+                <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+                    <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%]">
+                        <div className="absolute top-4 right-4 cursor-pointer"
+                            onClick={() => setOpen(false)}
+                        >
+                            <Image src={"/close.png"} alt="" width={14} height={14} />
+                        </div>
+                        <Form />
+                    </div>
+                </div>
+            }
         </>
     )
 }
