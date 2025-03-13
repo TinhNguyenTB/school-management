4511d61ff4320eda536a671b00cb859de2756e0a
TinhNguyenTB <tinh271103@gmail.com>
2025-03-13 09:20:39 +0700

    Create Prisma Seed

---
+++ prisma/migrations/20250313014405_allow_null_supervisor/migration.sql
@@ -0,0 +1,14 @@
+-- DropForeignKey
+ALTER TABLE "Class" DROP CONSTRAINT "Class_supervisorId_fkey";
+
+-- AlterTable
+ALTER TABLE "Class" ALTER COLUMN "supervisorId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Student" ALTER COLUMN "img" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Teacher" ALTER COLUMN "img" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Class" ADD CONSTRAINT "Class_supervisorId_fkey" FOREIGN KEY ("supervisorId") REFERENCES "Teacher"("id") ON DELETE SET NULL ON UPDATE CASCADE;
