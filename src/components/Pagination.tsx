8e1d08ad1316c785a7cd98746acb422d3fb760dd
TinhNguyenTB <tinh271103@gmail.com>
2025-03-13 13:48:20 +0700

    Prisma Pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,19 +1,48 @@
+'use client'
 
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { useRouter } from "next/navigation";
 
-const Pagination = () => {
+type PaginationProps = {
+    page: number;
+    count: number;
+}
+
+const Pagination = ({ page, count }: PaginationProps) => {
+    const router = useRouter();
+
+    const totalPages = Math.ceil(count / ITEM_PER_PAGE); // lam tron len
+
+    const changePage = (newPage: number) => {
+        const params = new URLSearchParams(window.location.search);
+        params.set("page", newPage.toString());
+        router.push(`${window.location.pathname}?${params}`)
+    }
+
     return (
         <div className="p-4 flex items-center justify-between text-gray-500">
-            <button className="py-2 px-4 rounded-md text-xs bg-slate-200 font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+            <button
+                disabled={page === 1}
+                onClick={() => changePage(page - 1)}
+                className="py-2 px-4 rounded-md text-xs bg-slate-200 font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
                 Prev
             </button>
             <div className="flex items-center gap-2 text-sm">
-                <button className="px-2 rounded-sm bg-mySky">1</button>
-                <button className="px-2 rounded-sm">2</button>
-                <button className="px-2 rounded-sm">3</button>
-                <button className="px-2 rounded-sm">...</button>
-                <button className="px-2 rounded-sm">10</button>
+                {Array.from({ length: totalPages }, (_, index) => {
+                    const pageIndex = index + 1;
+                    return (
+                        <button key={pageIndex}
+                            onClick={() => changePage(pageIndex)}
+                            className={`px-2 rounded-sm ${page === pageIndex ? "bg-mySky" : ""} `}>
+                            {pageIndex}
+                        </button>
+                    )
+                })}
             </div>
-            <button className="py-2 px-4 rounded-md text-xs bg-slate-200 font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+            <button
+                disabled={page === totalPages}
+                onClick={() => changePage(page + 1)}
+                className="py-2 px-4 rounded-md text-xs bg-slate-200 font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
                 Next
             </button>
         </div>
